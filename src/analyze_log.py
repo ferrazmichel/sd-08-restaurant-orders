d8c0c4da3afdf68156c1ca3574745b2c9ab6ae52
paulo-lins <paulolins100@gmail.com>
2021-09-28 12:22:19 -0300

    iniciando-requisito-1

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,19 @@
+import csv
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    #raise NotImplementedError
+    #Qual o prato mais pedido por 'maria'?
+    #maria está na primeira posição
+
+    with open(path_to_file) as csv_file:
+        csv_reader = tuple(csv.reader(csv_file, delimiter=','))
+        print(csv_reader)
+        for row in csv_reader:
+            prato_maria = {}
+            if row[0] == 'maria':
+                prato_maria['maria'] = row[1]
+        print(prato_maria)
+
+
+analyze_log('./data/orders_1.csv')
