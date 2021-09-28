1c4017d84887d58761e3d3896606970e5b4fab43
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-09-28 20:34:26 -0300

    starting restaurant orders project

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,22 @@
+import csv
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    if not path_to_file or not path_to_file.endswith(".csv"):
+        raise ValueError("Path to file is invalid")
+
+    info_list = []
+
+    try:
+        with open(path_to_file, "r") as csv_file:
+            # ñ precisa ler com DictReader, reader retorna cada linha um array
+            # csv_reader = csv.DictReader(csv_file, delimiter=",")
+            csv_reader = csv.reader(csv_file, delimiter=",")
+            for info in csv_reader:
+                info_list.append(info)
+        return info_list
+    except FileNotFoundError:
+        raise FileNotFoundError("File not found")
+
+
+print(analyze_log("./data/orders_1.csv"))
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,67 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append((costumer, order, day))
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        foods = {}
+        for order in self.orders:
+            if order[0] == costumer:
+                try:
+                    foods[order[1]] += 1
+                except KeyError:
+                    foods[order[1]] = 1
+        # source: https://www.kite.com/python/answers/how-to-find-the-max-
+        # value-in-a-dictionary-in-python#:~:text=values()%20to%20find%20the
+        # ,max%20value%20of%20the%20dictionary.
+        return max(foods, key=foods.get)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        all_foods = set()
+        costumer_foods = set()
+        # por ser um set nunca adiciona repetidos e ao
+        # fazer a diferença entre os dois conjuntos terá os nunca pedidos
+        for order in self.orders:
+            all_foods.add(order[1])
+            if order[0] == costumer:
+                costumer_foods.add(order[1])
+        return all_foods - costumer_foods
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        days_of_week = set()
+        costumer_days = set()
+        for order in self.orders:
+            days_of_week.add(order[2])
+            if order[0] == costumer:
+                costumer_days.add(order[2])
+        return days_of_week - costumer_days
 
     def get_busiest_day(self):
-        pass
+        days = {}
+        for order in self.orders:
+            try:
+                days[order[2]] += 1
+            except KeyError:
+                days[order[2]] = 1
+        # source: https://www.kite.com/python/answers/how-to-find-the-max-
+        # value-in-a-dictionary-in-python#:~:text=values()%20to%20find%20the
+        # ,max%20value%20of%20the%20dictionary.
+        return max(days, key=days.get)
 
     def get_least_busy_day(self):
+        days = {}
+        for order in self.orders:
+            try:
+                days[order[2]] += 1
+            except KeyError:
+                days[order[2]] = 1
+        # source: https://www.kite.com/python/answers/how-to-find-the-max-
+        # value-in-a-dictionary-in-python#:~:text=values()%20to%20find%20the
+        # ,max%20value%20of%20the%20dictionary.
+        return min(days, key=days.get)
         pass
