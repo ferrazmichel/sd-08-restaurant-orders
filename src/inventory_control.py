faf48d65931c5be3d213f77f7a2ac7d1b8a6ba7d
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-09-29 14:26:47 -0300

    alterações parciais em inventory_control para merge da master

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -25,14 +25,21 @@ class InventoryControl:
         for key in self.INGREDIENTS.keys():
             self.available_dishes.add(key)
 
+    def remove_available_dishes_by_ingredient(self, ingredient):
+        for key, value in self.INGREDIENTS.items():
+            if ingredient in value:
+                self.available_dishes.discard(key)
+
     def remove_available_dishes(self, order):
         for ingredient in self.INGREDIENTS[order]:
-            if (
-                self.ingredients_to_buy[ingredient] + 1
-                > self.MINIMUM_INVENTORY[ingredient]
+            if ingredient in self.MINIMUM_INVENTORY:
+                if (
+                    self.ingredients_to_buy[ingredient] + 1
+                    > self.MINIMUM_INVENTORY[ingredient]
 
-            ):
-                self.available_dishes.discard(order)
+                ):
+                    self.available_dishes.discard(order)
+                    self.remove_available_dishes_by_ingredient(ingredient)
 
     def add_new_order(self, costumer, order, day):
         if order not in self.available_dishes:
@@ -40,7 +47,8 @@ class InventoryControl:
         self.remove_available_dishes(order)
         if order in self.available_dishes:
             for ingredient in self.INGREDIENTS[order]:
-                self.ingredients_to_buy[ingredient] += 1
+                if ingredient in self.MINIMUM_INVENTORY:
+                    self.ingredients_to_buy[ingredient] += 1
 
     def get_quantities_to_buy(self):
         return self.ingredients_to_buy
