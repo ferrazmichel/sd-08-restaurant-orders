8ac09c08c052237454b75c9628ea6c96b476d5a2
Daniel <danieldsrd@gmail.com>
2021-09-29 16:37:28 -0300

    Correcao

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -12,12 +12,16 @@ def get_all_orders(path_to_file):
             food.add(row[1])
             day.add(row[2])
             if row[0] in person_dict:
-                # append the new number to the existing array at this slot
                 person_dict[row[0]].append((row[1], row[2]))
             else:
-                # create a new array in this slot
                 person_dict[row[0]] = [(row[1], row[2])]
     return person_dict, food, day
+
+
+def write_file(data):
+    with open("data/mkt_campaign.txt", mode="w") as f:
+        for item in data:
+            f.write("%s\n" % item)
 
 
 def analyze_log(path_to_file):
@@ -27,7 +31,6 @@ def analyze_log(path_to_file):
         maria_eats.append(row[0])
     maria_dict = {i: maria_eats.count(i) for i in maria_eats}
     maria_result = max(maria_dict, key=maria_dict.get)
-    print(maria_result)
 
     arnaldo_result = 0
     for row in person_dict["arnaldo"]:
@@ -43,8 +46,6 @@ def analyze_log(path_to_file):
     joao_result_food = food.difference(joao_foodoff)
     joao_result_day = day.difference(joao_dayoff)
 
-    with open("data/mkt_campaign.txt", mode="w") as f:
-        f.write(
-            f"{str(maria_result)}\n{str(arnaldo_result)}\n"
-            f"{str(joao_result_food)}\n{str(joao_result_day)}"
-        )
+    data = [maria_result, arnaldo_result, joao_result_food, joao_result_day]
+
+    write_file(data)
