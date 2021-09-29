69681ba94d57f5cd7f6aad9b2ca8bd00679edaa1
Rita Jeveaux <ritajeveaux@gmail.com>
2021-09-29 13:34:28 -0300

    Req1

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,24 @@
+import csv
+from src.track_orders import TrackOrders
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    output_file = "data/mkt_campaign.txt"
+    contents = []
+    tracker = TrackOrders()
+    with open(path_to_file, mode='r') as read_file:
+        result = csv.reader(read_file, delimiter="\n")
+        print(result)
+        print("AI")
+        for row in result:
+            contents.append(row)
+        print(contents)
+    with open(output_file, mode='w') as write_file:
+        print("UI")
+        print(
+            tracker.get_most_ordered_dish_per_costumer("maria"),
+            tracker.get_dish_per_costumer("arnaldo", "hamburguer"),
+            tracker.get_never_ordered_per_costumer("joao"),
+            tracker.get_days_never_visited_per_costumer("joao"),
+            sep="\n", file=write_file
+            )
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,18 +1,35 @@
+# from collections import Counter
+
+
 class TrackOrders:
+
+    def __init__(self):
+        self.orders = {}
+
     def __len__(self):
+        return len(self.orders)
+
+    def get_dish_per_costumer(self, costumer):
         pass
 
     def add_new_order(self, costumer, order, day):
         pass
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        result = max(self.orders[costumer]["dishes"])  # interação - lista de lista
+        return result.key
 
     def get_never_ordered_per_costumer(self, costumer):
         pass
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        alldays = ([
+                "segunda-feira",
+                "terça-feira",
+                "sabado",
+            ])
+        result = self.orders[costumer]["days"]  # fazer uma iteração
+        return alldays.difference(result.key)
 
     def get_busiest_day(self):
         pass
