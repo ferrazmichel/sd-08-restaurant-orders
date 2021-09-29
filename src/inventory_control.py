c86da677be3d6da49a62968b42a71b0d5781888a
hugobrr <hugobrr@gmail.com>
2021-09-29 14:22:53 -0300

    terceiro incopmpleto

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,29 +1,52 @@
 class InventoryControl:
     INGREDIENTS = {
-        'hamburguer': ['pao', 'carne', 'queijo'],
-        'pizza': ['massa', 'queijo', 'molho', 'tomate'],
-        'queijo-quente': ['pao', 'queijo', 'queijo'],
-        'misto-quente': ['pao', 'queijo', 'presunto'],
-        'bauru': ['pao', 'queijo', 'presunto', 'tomate'],
-        'coxinha': ['massa', 'frango'],
+        "hamburguer": ["pao", "carne", "queijo"],
+        "pizza": ["massa", "queijo", "molho"],
+        "queijo-quente": ["pao", "queijo", "queijo"],
+        "misto-quente": ["pao", "queijo", "presunto"],
+        "coxinha": ["massa", "frango"],
     }
     MINIMUM_INVENTORY = {
-        'pao': 50,
-        'carne': 50,
-        'queijo': 100,
-        'molho': 50,
-        'presunto': 50,
-        'massa': 50,
-        'frango': 50,
+        "pao": 50,
+        "carne": 50,
+        "queijo": 100,
+        "molho": 50,
+        "presunto": 50,
+        "massa": 50,
+        "frango": 50,
     }
-    # data = []
 
     def __init__(self):
+        self.data = []
+        self.ingredients = self.MINIMUM_INVENTORY.copy()
         pass
 
     def add_new_order(self, costumer, order, day):
-        # data.append([costumer, order, day])
-        pass
+        self.data.append([costumer, order, day])
+        list_ingredients = self.INGREDIENTS[order]
+        for item in list_ingredients:
+            if self.ingredients[item] > 0:
+                self.ingredients[item] -= 1
+            else:
+                return False
 
     def get_quantities_to_buy(self):
-        pass
+        result = {}
+        for item in self.MINIMUM_INVENTORY:
+            print(self.MINIMUM_INVENTORY[item], self.ingredients[item])
+            result[item] = (
+                self.MINIMUM_INVENTORY[item] - self.ingredients[item]
+            )
+        return result
+
+    def get_available_dishes(self):
+        result = set()
+        set_of_ingradients = set(self.ingredients)
+        recipes = self.INGREDIENTS
+        for recipe in recipes:
+            set_rec = set(recipes[recipe])
+            # print(set_rec, set_of_ingradients)
+            # print(set_rec.issubset(set_of_ingradients))
+            if set_rec.issubset(set_of_ingradients):
+                result.add(recipe)
+        return result
