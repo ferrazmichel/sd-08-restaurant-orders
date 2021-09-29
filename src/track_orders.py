2e7ca58cb15a12c462cc358310462a2f9e177d35
paulo-lins <paulolins100@gmail.com>
2021-09-29 20:47:01 -0300

    add_new_order
    Usando a palavra-chave “self”, podemos acessar
    os atributos e métodos da classe em python
    Ele vincula os atributos aos argumentos fornecidos
    Após entender isto ficou mais fácil fazer os métodos
    "conversarem".métodos da classe em python
        # Ele vincula os atributos aos argumentos fornecidos
        # Após entender isto ficou mais fácil fazer os métodos
        # "conversarem".

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,5 +1,10 @@
 class TrackOrders:
-
+    # self representa a instância da classe.
+    # Usando a palavra-chave “self”, podemos acessar
+    # os atributos e métodos da classe em python
+    # Ele vincula os atributos aos argumentos fornecidos
+    # Após entender isto ficou mais fácil fazer os métodos
+    # "conversarem".  
     def __init__(self):
         self.orders = []
 
@@ -7,7 +12,9 @@ class TrackOrders:
         return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        """Adiciona o cliente, o pedido e o dia à
+        lista orders"""
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
         """Retorna um dict com o prato mais pedido pelo cliente"""
