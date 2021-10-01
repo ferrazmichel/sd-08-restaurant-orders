72d19d980ca4b13fbeb790eb7ee3816445f8a341
Franco <jnpefo@gmail.com>
2021-10-01 18:47:02 -0300

    Fix do Req 1

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,20 +1,10 @@
 import csv
 from statistics import mode
-def analyze_log(path_to_file):
-    nome = {}
-    pedido = set()
-    dia = set()
 
-    with open(path_to_file, "r") as file:
-        data = csv.reader(file, delimiter=",", quotechar='"')
-        for valor in data:
-            pedido.add(valor[1])
-            dia.add(valor[2])
-            if valor[0] not in nome:
-                nome[valor[0]] = [(valor[1], valor[2])]
-            else:
-                nome[valor[0]].append((valor[1], valor[2]))
-    
+
+def analyze_log(path_to_file):
+    nome, pedido, dia = get_dados(path_to_file)
+
     maria = []
     for valor in nome["maria"]:
         maria.append(valor[0])
@@ -32,9 +22,27 @@ def analyze_log(path_to_file):
         joao_dia.add(valor[1])
 
     string = (f"{maria_maior}\n{arnaldo}\n"
-    f"{joao_pediu.symmetric_difference(pedido)}\n"
-    f"{joao_dia.symmetric_difference(dia)}")
+        f"{joao_pediu.symmetric_difference(pedido)}\n"
+        f"{joao_dia.symmetric_difference(dia)}")
 
     file = open("data/mkt_campaign.txt", mode="w")
     file.write(string)
     file.close
+
+
+def get_dados(path_to_file):
+    nome = {}
+    pedido = set()
+    dia = set()
+
+    with open(path_to_file, "r") as file:
+        data = csv.reader(file, delimiter=",", quotechar='"')
+        for valor in data:
+            pedido.add(valor[1])
+            dia.add(valor[2])
+            if valor[0] not in nome:
+                nome[valor[0]] = [(valor[1], valor[2])]
+            else:
+                nome[valor[0]].append((valor[1], valor[2]))
+
+    return (nome, pedido, dia)
