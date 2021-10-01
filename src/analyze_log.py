0f8a794c347726ef95c5a40e9189920702b6ebe3
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-10-01 18:13:51 -0300

    more_request_by_Maria

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,28 @@
+import csv
+
+
+def more_request_by_Maria(orders_list):
+    count = {}
+    most_frequent = orders_list[0]["Comida"]
+
+    for order in orders_list:
+        name, food = (order["Nome"], order["Comida"])
+        if name == "maria":
+            if food not in count:
+                count[food] = 1
+            else:
+                count[food] += 1
+                if count[food] > count[most_frequent]:
+                    most_frequent = food
+    return most_frequent
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    with open(path_to_file) as file:
+        orders_reader = csv.reader(file, delimiter=",")
+        orders_list = [{"Nome": row[0], "Comida": row[1], "dia": row[2]}
+                       for row in orders_reader]
+    return more_request_by_Maria(orders_list)
+
+
+print(analyze_log("data/orders_1.csv"))
