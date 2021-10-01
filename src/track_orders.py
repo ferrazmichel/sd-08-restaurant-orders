1702db4cee6ea42634d7d60c92c5a99ef221a3d9
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-09-30 21:08:23 -0300

    feat: complete requirement 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,47 @@
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = {}
+        self.menu = set()
+        self.work_days = set()
+        self.days = []
+
     def __len__(self):
-        pass
+        length = 0
+        for _, orders in self.orders.items():
+            length += len(orders)
+        return length
 
     def add_new_order(self, costumer, order, day):
-        pass
+        if costumer in self.orders:
+            self.orders[costumer].append((order, day))
+        else:
+            self.orders[costumer] = [(order, day)]
+        self.menu.add(order)
+        self.work_days.add(day)
+        self.days.append(day)
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        ordered_by_customer = Counter(self.orders[costumer])
+        most_ordered_by_customer = ordered_by_customer.most_common(1)[0][0][0]
+        return most_ordered_by_customer
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        ordered_by_customer = set(order[0] for order in self.orders[costumer])
+        never_ordered_by_customer = self.menu.difference(ordered_by_customer)
+        return never_ordered_by_customer
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        customer_attendace = set(order[1] for order in self.orders[costumer])
+        customer_absence = self.work_days.difference(customer_attendace)
+        return customer_absence
 
     def get_busiest_day(self):
-        pass
+        busiest_day = Counter(self.days).most_common(1)[0][0]
+        return busiest_day
 
     def get_least_busy_day(self):
-        pass
+        least_busy_day = Counter(self.days).most_common()[-1][0]
+        return least_busy_day
