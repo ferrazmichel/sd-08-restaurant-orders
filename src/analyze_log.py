b559e6edc6d02dca3a40cf3891c1d644bc4c131d
Seu nome <luiza.dilly@gmail.com>
2021-10-02 17:51:34 -0300

    fix flake

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,5 +1,6 @@
 import csv
 
+
 def most_ordered_meal(client, orders):
     food = {}
 
@@ -12,6 +13,7 @@ def most_ordered_meal(client, orders):
                 food[requested_food['order']] += 1
     return max(food, key=food.get)
 
+
 def how_many_times_burguer_was_ordered(client, orders):
     food = {}
 
@@ -23,45 +25,41 @@ def how_many_times_burguer_was_ordered(client, orders):
                 food[requested_food['order']] += 1
     return food['hamburguer']
 
+
 def food_never_ordered(client, orders):
     order = set()
-
     all_orders = set()
-    
     for requested_food in orders:
         all_orders.add(requested_food['order'])
     for requested_food in orders:
         if client == requested_food['client']:
             order.add(requested_food['order'])
-    
     return all_orders - order
+
 
 def never_went_to_diner(client, orders):
     date = set()
-
     all_date = set()
-    
     for requested_food in orders:
         all_date.add(requested_food['day'])
     for requested_food in orders:
         if client == requested_food['client']:
             date.add(requested_food['day'])
-    
     return all_date - date
 
+
 def read_csv_file(path):
-
-    data =[]
-
+    data = []
     with open(path) as file:
         read = csv.reader(file, delimiter=',', quotechar='"')
         for row in read:
-                client, order, day = row
-                data.append(
-                    {'client': client, 'order': order, 'day': day}
-                )
+            client, order, day = row
+            data.append(
+                {'client': client, 'order': order, 'day': day}
+            )
 
     return data
+
 
 def write_file(content):
     with open('data/mkt_campaign.txt', 'w') as file:
@@ -69,14 +67,13 @@ def write_file(content):
 
 
 def analyze_log(path_to_file):
-    
     data = read_csv_file(path_to_file)
 
-    maria_order = most_ordered_meal('maria', data)
-    arnaldo_order = how_many_times_burguer_was_ordered('arnaldo', data)
-    joao_not_ordered = food_never_ordered('joao', data)
+    maria = most_ordered_meal('maria', data)
+    arnaldo = how_many_times_burguer_was_ordered('arnaldo', data)
+    joao = food_never_ordered('joao', data)
     joao_never_went = never_went_to_diner('joao', data)
 
-    converted_str = f"{maria_order}\n{arnaldo_order}\n{joao_not_ordered}\n{joao_never_went}\n"
+    converted_str = f"{maria}\n{arnaldo}\n{joao}\n{joao_never_went}\n"
 
     write_file(converted_str)
