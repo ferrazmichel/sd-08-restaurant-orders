7d60879cc959868c15433167f2a2f131c8e6d3b9
Fabio Ferreira <fabio35547@gmail.com>
2021-10-02 10:17:14 -0300

    Requisito 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,59 @@
+from statistics import mode
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self.pedidos = []
+
     def __len__(self):
-        pass
+        return len(self.pedidos)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.pedidos.append({
+            'costumer': costumer,
+            'order': order,
+            'day': day
+        })
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return mode(
+            pedido['order']
+            for pedido in self.pedidos
+            if pedido['costumer'] == costumer
+        )
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        todos_produtos = set([pedido['order'] for pedido in self.pedidos])
+        produtos_cliente = set([
+            pedido['order']
+            for pedido in self.pedidos
+            if pedido['costumer'] == costumer
+        ])
+
+        return todos_produtos.difference(produtos_cliente)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        dias_da_semana = set([pedido['day'] for pedido in self.pedidos])
+        dias_cliente_nunca_foi_na_lanchonete = set([
+            pedido['day']
+            for pedido in self.pedidos
+            if pedido['costumer'] == costumer
+        ])
+
+        return dias_da_semana.difference(dias_cliente_nunca_foi_na_lanchonete)
 
     def get_busiest_day(self):
-        pass
+        return mode(
+            pedido['day'] for pedido in self.pedidos
+        )
 
     def get_least_busy_day(self):
-        pass
+        # https://pt.stackoverflow.com/questions/456224/obter-o-elemento-mais-frequente-e-menos-frequente-de-uma-lista-al%C3%A9m-do-maior-e
+        frequencias = Counter(
+            pedido['day'] for pedido in self.pedidos
+        ).most_common()
+
+        menos_frequente = frequencias[-1]
+
+        return menos_frequente[0]
