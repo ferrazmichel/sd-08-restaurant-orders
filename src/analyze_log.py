9b692ece3ddf7aee57a8d5113b7e567fa27395cf
Seu nome <luiza.dilly@gmail.com>
2021-10-02 17:43:37 -0300

    req 1 done

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,82 @@
+import csv
+
+def most_ordered_meal(client, orders):
+    food = {}
+
+    for requested_food in orders:
+        # print(requested_food)
+        if client == requested_food['client']:
+            if requested_food['order'] not in food:
+                food[requested_food['order']] = 1
+            else:
+                food[requested_food['order']] += 1
+    return max(food, key=food.get)
+
+def how_many_times_burguer_was_ordered(client, orders):
+    food = {}
+
+    for requested_food in orders:
+        if client == requested_food['client']:
+            if requested_food['order'] not in food:
+                food[requested_food['order']] = 1
+            else:
+                food[requested_food['order']] += 1
+    return food['hamburguer']
+
+def food_never_ordered(client, orders):
+    order = set()
+
+    all_orders = set()
+    
+    for requested_food in orders:
+        all_orders.add(requested_food['order'])
+    for requested_food in orders:
+        if client == requested_food['client']:
+            order.add(requested_food['order'])
+    
+    return all_orders - order
+
+def never_went_to_diner(client, orders):
+    date = set()
+
+    all_date = set()
+    
+    for requested_food in orders:
+        all_date.add(requested_food['day'])
+    for requested_food in orders:
+        if client == requested_food['client']:
+            date.add(requested_food['day'])
+    
+    return all_date - date
+
+def read_csv_file(path):
+
+    data =[]
+
+    with open(path) as file:
+        read = csv.reader(file, delimiter=',', quotechar='"')
+        for row in read:
+                client, order, day = row
+                data.append(
+                    {'client': client, 'order': order, 'day': day}
+                )
+
+    return data
+
+def write_file(content):
+    with open('data/mkt_campaign.txt', 'w') as file:
+        file.write(content)
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
-    # 
+    
+    data = read_csv_file(path_to_file)
+
+    maria_order = most_ordered_meal('maria', data)
+    arnaldo_order = how_many_times_burguer_was_ordered('arnaldo', data)
+    joao_not_ordered = food_never_ordered('joao', data)
+    joao_never_went = never_went_to_diner('joao', data)
+
+    converted_str = f"{maria_order}\n{arnaldo_order}\n{joao_not_ordered}\n{joao_never_went}\n"
+
+    write_file(converted_str)
