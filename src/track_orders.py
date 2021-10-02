b353233cab98154077d71a6f85102bba5340d243
Aline Debastiani <debastianialine@gmail.com>
2021-10-02 19:51:44 -0300

    cumpre requisito 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,40 @@
+from src.analyze_log import (
+    most_requested_plate, never_ordered_plate, days_without_orders
+)
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return most_requested_plate(costumer, self.orders)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return never_ordered_plate(costumer, self.orders)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return days_without_orders(costumer, self.orders)
+
+    def orders_per_day(self):
+        days = {}
+        for order in self.orders:
+            if order[2] not in days:
+                days[order[2]] = 1
+            else:
+                days[order[2]] += 1
+        return days
 
     def get_busiest_day(self):
-        pass
+        days = self.orders_per_day()
+        return max(days, key=days.get)
 
     def get_least_busy_day(self):
-        pass
+        days = self.orders_per_day()
+        return min(days, key=days.get)
