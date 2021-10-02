b5f6a1a9d314a98eb64c7cbe564247cd2149ae76
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-10-02 15:07:22 -0300

    Req 2 done

---
+++ src/order_filters.py
@@ -0,0 +1,48 @@
+# 1 - Getting key with maximum value in dictionary:
+# https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
+
+def filter_by_order(orders, order):
+    return filter_orders(orders, 'order', order)
+
+
+def filter_by_client(orders, client):
+    return filter_orders(orders, 'client', client)
+
+
+def filter_orders(all_orders, field, value):
+    filtered_orders = list(filter(
+        lambda order: order[field] == value, all_orders
+    ))
+    return filtered_orders
+
+
+def get_values_from_field(all_orders, field):
+    values_list = []
+    for order in all_orders:
+        if order[field] not in values_list:
+            values_list.append(order[field])
+    return values_list
+
+
+def find_most_repeated_value(all_orders, field):
+    fieldValueDict = {}
+    for order in all_orders:
+        value = order[field]
+        if value not in fieldValueDict:
+            fieldValueDict[value] = 1
+        else:
+            fieldValueDict[value] += 1
+    return max(fieldValueDict, key=fieldValueDict.get)  # 1
+
+
+def find_less_repeated_value(all_orders, field):
+    fieldValueDict = {}
+    for order in all_orders:
+        value = order[field]
+        if value not in fieldValueDict:
+            fieldValueDict[value] = 1
+        else:
+            fieldValueDict[value] += 1
+    # return (all_orders)
+    # return get_values_from_field(all_orders, 'day')
+    return min(fieldValueDict, key=fieldValueDict.get)  # 1
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,46 @@
+import src.order_filters as f
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append(
+            {'client': costumer, 'order': order, 'day': day}
+        )
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        client_orders = f.filter_by_client(self.orders, costumer)
+        most_ordered = f.find_most_repeated_value(client_orders, 'order')
+        return most_ordered
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        client_orders = f.filter_by_client(self.orders, costumer)
+        client_dishes = f.get_values_from_field(client_orders, 'order')
+        all_dishes = f.get_values_from_field(self.orders, 'order')
+        client_didnt_order = set(all_dishes).difference(
+            set(client_dishes)
+        )  # 1
+        print('CLIENT_DISHES', client_dishes)
+        print('ALL_DISHES', all_dishes)
+        return client_didnt_order
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        client_orders = f.filter_by_client(self.orders, costumer)
+        days_client_did_go = f.get_values_from_field(client_orders, 'day')
+        week_days = f.get_values_from_field(self.orders, 'day')
+        days_client_didnt_go = set(week_days).difference(
+            set(days_client_did_go)
+        )  # 1
+        return days_client_didnt_go
 
     def get_busiest_day(self):
-        pass
+        busiest = f.find_most_repeated_value(self.orders, 'day')
+        return busiest
 
     def get_least_busy_day(self):
-        pass
+        return f.find_less_repeated_value(self.orders, 'day')
