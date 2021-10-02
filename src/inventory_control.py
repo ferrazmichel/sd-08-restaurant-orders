f1fbc2e27eab978c66e1b9c65ad2f541e176b0da
emassaki <massakihirayama@gmail.com>
2021-10-02 18:52:44 -0300

    Added req 3

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,25 +1,40 @@
 class InventoryControl:
     INGREDIENTS = {
-        'hamburguer': ['pao', 'carne', 'queijo'],
-        'pizza': ['massa', 'queijo', 'molho'],
-        'misto-quente': ['pao', 'queijo', 'presunto'],
-        'coxinha': ['massa', 'frango'],
+        "hamburguer": ["pao", "carne", "queijo"],
+        "pizza": ["massa", "queijo", "molho"],
+        "misto-quente": ["pao", "queijo", "presunto"],
+        "coxinha": ["massa", "frango"],
     }
     MINIMUM_INVENTORY = {
-        'pao': 50,
-        'carne': 50,
-        'queijo': 100,
-        'molho': 50,
-        'presunto': 50,
-        'massa': 50,
-        'frango': 50,
+        "pao": 50,
+        "carne": 50,
+        "queijo": 100,
+        "molho": 50,
+        "presunto": 50,
+        "massa": 50,
+        "frango": 50,
     }
 
     def __init__(self):
-        pass
+        self.orders = []
+        self.ingredients_stock = {
+            "pao": 50,
+            "carne": 50,
+            "queijo": 100,
+            "molho": 50,
+            "presunto": 50,
+            "massa": 50,
+            "frango": 50,
+        }
 
     def add_new_order(self, costumer, order, day):
-        pass
+        used_ingredients = self.INGREDIENTS[order]
+        for ingredient in used_ingredients:
+            self.ingredients_stock[ingredient] -= 1
+        self.orders.append([costumer, order, day])
 
     def get_quantities_to_buy(self):
-        pass
+        return {
+            ing: self.MINIMUM_INVENTORY[ing] - self.ingredients_stock[ing]
+            for ing in self.ingredients_stock
+        }
