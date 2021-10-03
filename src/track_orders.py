7c7352caea41617cb03489b1a9e5c3886af02c45
VitorLimaRios <vitorlimarios999@gmail.com>
2021-10-03 16:09:21 -0300

    adding req 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,46 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        foods = {}
+        for item in self.orders:
+            if item[0] == costumer:
+                foods[item[1]] = foods.get(item[1], 0) + 1
+        return max(foods, key=foods.get)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        foods = set()
+        customer_orders = set()
+        for item in self.orders:
+            foods.add(item[1])
+            if item[0] == costumer:
+                customer_orders.add(item[1])
+        return foods - customer_orders
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        days = set()
+        customer_days = set()
+        for item in self.orders:
+            days.add(item[2])
+            if item[0] == costumer:
+                customer_days.add(item[2])
+        return days - customer_days
 
     def get_busiest_day(self):
-        pass
+        days = {}
+        for item in self.orders:
+            days[item[2]] = days.get(item[2], 0) + 1
+        return max(days, key=days.get)
 
     def get_least_busy_day(self):
-        pass
+        days = {}
+        for item in self.orders:
+            days[item[2]] = days.get(item[2], 0) + 1
+        return min(days, key=days.get)
