d71ce86eace42c151372712f2df8a3d38fd3b71c
tandy <tandyfw@gmail.com>
2021-10-03 10:36:55 -0300

    Requisito 2 concluido 5/7

--- src/track_orders.py
+++ src/track_orders.py
@@ -20,10 +20,24 @@ class TrackOrders:
         return higher_order
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        costumer_orders = [order for order in self.orders if costumer in order]
+        orders = [order[1] for order in costumer_orders]
+        costumer_orders_dict = {i for i in orders}
+
+        general_orders = [order[1] for order in self.orders]
+        general_orders_dict = {i for i in general_orders}
+
+        return general_orders_dict - costumer_orders_dict
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        costumer_day = [day for day in self.orders if costumer in day]
+        days = [day[2] for day in costumer_day]
+        costumer_days_dict = {i for i in days}
+
+        general_days = [day[2] for day in self.orders]
+        general_days_dict = {i for i in general_days}
+
+        return general_days_dict - costumer_days_dict
 
     def get_busiest_day(self):
         pass
