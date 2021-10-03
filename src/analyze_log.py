462985dcf342a73308c6b276f1aef49139f0c9c3
tandy <tandyfw@gmail.com>
2021-10-03 08:24:49 -0300

    Requisito 1 concluido 2/3

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,12 @@
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    with open(path_to_file) as file:
+        lines = file.readlines()
+        lines = [line.rstrip() for line in lines if 'maria' in line]
+        my_dict = {i: lines.count(i) for i in lines}
+        max_key = max(my_dict, key=my_dict.get)
+        maria = max_key.split(',')[1]
+        result = open('data/mkt_campaign.txt', 'a')
+        result.write(maria)
+
+
+# print(analyze_log('/home/tandy/trybe/projects/computer-science/sd-08-restaurant-orders/data/orders_1.csv'))
