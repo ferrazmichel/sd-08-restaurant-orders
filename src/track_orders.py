cf995cf18e81780518ab17653536a15bb2af101a
Anderson Santana <anderslash@gmail.com>
2021-10-03 15:43:29 -0300

    feat: track orders getnever ordered per costumer

--- src/track_orders.py
+++ src/track_orders.py
@@ -3,23 +3,27 @@ from collections import Counter
 
 class TrackOrders:
     def __init__(self):
-        self.ordered_products = []
+        self.ordered_dishes = []
 
     def __len__(self):
-        return len(self.ordered_products)
+        return len(self.ordered_dishes)
 
     def add_new_order(self, costumer, order, day):
-        return self.ordered_products.append((costumer, order, day))
+        return self.ordered_dishes.append((costumer, order, day))
 
     def get_most_ordered_dish_per_costumer(self, costumer):
         order_list = list(
-            record[1] for record in self.ordered_products
+            record[1] for record in self.ordered_dishes
             if record[0] == costumer
         )
         return Counter(order_list).most_common()[0][0]
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        dishes = set(record[1] for record in self.ordered_dishes)
+        dishes_per_costumer = set(
+            record[1] for record in self.ordered_dishes
+            if record[0] == costumer)
+        return dishes.difference(dishes_per_costumer)
 
     def get_days_never_visited_per_costumer(self, costumer):
         pass
