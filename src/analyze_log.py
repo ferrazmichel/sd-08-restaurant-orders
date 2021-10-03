5d7837e58a96de555bcbb29f6469dc700e23bf0a
Tiago <tiagaoalb@gmail.com>
2021-10-03 20:10:35 -0300

    add favorite and repeated dishes functions

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,4 +1,22 @@
+def favorite_dish(name, orders):
+    favorite_dish = ""
+    for row in orders:
+        if row[0] == name:
+            favorite_dish = row[1]
+            break
+    return favorite_dish
+
+
+def repeated_dishes(name, dish, orders):
+    client_order = set()
+    for row in orders:
+        if row[0] == name and row[1] == dish:
+            client_order.add(row[1])
+    return len(client_order)
+
+
+
+
+
 def analyze_log(path_to_file):
-    for row in path_to_file:
-        return row
     raise NotImplementedError
