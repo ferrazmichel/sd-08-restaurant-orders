4b8a406490f2ad15bd47f36c8f60b3adf233b152
Digo-Castro <digo_castro@outlook.com>
2021-10-03 15:18:46 -0300

    [Digo-Castro][Restaurant-Orders] - Req 1 - OK

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,31 @@
+import csv
+from statistics import mode
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    maria_orders = []
+    joao_orders = set()
+    joao_days = set()
+    counter = 0
+    days = set()
+    meals = set()
+
+    with open(path_to_file) as file:
+        data = csv.reader(file, delimiter=",", quotechar='"')
+
+        for order in data:
+            meals.add(order[1])
+            days.add(order[2])
+            if order[0] == "maria":
+                maria_orders.append(order[1])
+            if order[0] == "joao":
+                joao_orders.add(order[1])
+                joao_days.add(order[2])
+            if order[0] == "arnaldo" and order[1] == "hamburguer":
+                counter += 1
+
+    report = open('data'+'/'+'mkt_campaign.txt', 'w')
+    report.write(f"{mode(maria_orders)}\n")
+    report.write(f"{counter}\n")
+    report.write(f"{meals - joao_orders}\n")
+    report.write(f"{days - joao_days}\n")
