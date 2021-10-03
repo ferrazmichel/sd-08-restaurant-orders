05b6688bda1634af5651ddcd5f3a39016d3a2d66
VitorLimaRios <vitorlimarios999@gmail.com>
2021-10-03 16:13:55 -0300

    fix linter

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -9,10 +9,12 @@ def maria_most_ordered_dishes(item, dishes):
 def arnaldo_hamburguer_quantity(item):
     return item[0] == "arnaldo" and item[1] == "hamburguer"
 
+
 def joao_missing_dishes_and_days(item, joao_orders, joao_days):
     if item[0] == "joao":
         joao_orders.add(item[1])
         joao_days.add(item[2])
+
 
 def analyze_log(path_to_file):
     try:
@@ -34,9 +36,11 @@ def analyze_log(path_to_file):
                 arnaldo_hamburguer += 1
             joao_missing_dishes_and_days(item, joao_orders, joao_days)
 
-        # https://www.kite.com/python/answers/how-to-find-the-max-value-in-a-dictionary-in-python
+        """
+        https://www.kite.com/python/answers/how-to-find-the-max-value-in-a-dictionary-in-python
+        """
         maria = max(maria_dishes, key=maria_dishes.get)
-    
+
         joao = [(foods - joao_orders), (days - joao_days)]
 
         # https://www.pythontutorial.net/python-basics/python-write-text-file/
@@ -46,4 +50,6 @@ def analyze_log(path_to_file):
             f.write(f'{joao[0]}\n')
             f.write(f'{joao[1]}')
     except FileNotFoundError:
-        raise FileNotFoundError(f"No such file or directory: " "'{}'".format(path_to_file))
+        raise FileNotFoundError(
+            "No such file or directory: " "'{}'".format(path_to_file)
+        )
