7a7a33669ffc89661a3c71a43747983ef9733ef2
Paulo Simões <paulohbsimoes@gmail.com>
2021-10-03 18:39:06 -0300

    feat: add req 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,34 @@
+from os import confstr_names
+from src.Analyzer import Analyzer
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+        self.count = 0
+        self.analyzer = Analyzer(self.orders)
+
     def __len__(self):
-        pass
+        return self.count
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append({"client": costumer, "item": order, "day": day})
+        self.count += 1
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return self.analyzer.get_most_ordered_item(costumer)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return self.analyzer.get_not_tried_items(costumer)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return self.analyzer.get_client_not_show_up_days(costumer)
 
     def get_busiest_day(self):
-        pass
+        return self.analyzer.get_busiest_day()
 
     def get_least_busy_day(self):
-        pass
+        return self.analyzer.get_less_busy_day()
+
+    def get_order_frequency_per_costumer(self, customer_name, item_name):
+        return self.analyzer.get_client_item_count(customer_name, item_name)
