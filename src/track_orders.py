ed97e4a7e843f564a821fde7a8f70ec5b02063b6
victor Farias <victorfarias.tv@gmail.com>
2021-10-04 15:08:52 -0300

    Requisito 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,62 @@
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        return self.orders.append((costumer, order, day))
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        dish_list = []
+
+        for item in self.orders:
+            if item[0] == costumer:
+                dish_list.append(item[1])
+
+        most_eaten_dish, _ = Counter(dish_list).most_common()[0]
+        return most_eaten_dish
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        dishes = set()
+        ordered_dish = set()
+
+        for item in self.orders:
+            dishes.add(item[1])
+            if item[0] == costumer:
+                ordered_dish.add(item[1])
+
+        return dishes.difference(ordered_dish)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        days = set()
+        visit_days = set()
+
+        for item in self.orders:
+            days.add(item[2])
+            if item[0] == costumer:
+                visit_days.add(item[2])
+
+        return days.difference(visit_days)
 
     def get_busiest_day(self):
-        pass
+        days = []
+        for item in self.orders:
+            days.append(item[2])
+
+        most_visit_day, _ = Counter(days).most_common()[0]
+
+        return most_visit_day
 
     def get_least_busy_day(self):
-        pass
+        days = []
+        for item in self.orders:
+            days.append(item[2])
+
+        least_visit_day, _ = Counter(days).most_common()[-1]
+
+        return least_visit_day
