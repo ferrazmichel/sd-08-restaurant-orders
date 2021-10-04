97d70aa98ac7ece64cbc40b0a697d3e93a5676c2
Ediberto Oliveira <edibertooliveira@aol.com>
2021-10-04 17:51:07 -0300

    feat:  requirement 02

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,52 @@
+from src.analyze_log import plate
+
+
 class TrackOrders:
+
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append((costumer, order, day))
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return plate.favorite(costumer, self.orders)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return plate.never_ordered(costumer, self.orders)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return plate.days_without_orders(costumer, self.orders)
 
     def get_busiest_day(self):
-        pass
+        days_count_order = dict()
+        max_order = 0
+        busiest_day = ""
+        for order in self.orders:
+            if order[2] in days_count_order:
+                days_count_order[order[2]] += 1
+            else:
+                days_count_order[order[2]] = 1
+
+            if max_order < days_count_order[order[2]]:
+                busiest_day = order[2]
+                max_order = days_count_order[order[2]]
+        return busiest_day
 
     def get_least_busy_day(self):
-        pass
+        days_count_order = dict()
+        min_order = 999
+        least_busy_day = ""
+        for order in self.orders:
+            if order[2] in days_count_order:
+                days_count_order[order[2]] += 1
+            else:
+                days_count_order[order[2]] = 1
+
+            if min_order >= days_count_order[order[2]]:
+                least_busy_day = order[2]
+                min_order = days_count_order[order[2]]
+        return least_busy_day
