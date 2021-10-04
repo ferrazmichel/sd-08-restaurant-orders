c095f5496606a948d54aa46cf633abde4362aa4f
gabrielDonnantuoni <gabrieldonnantuoni@gmail.com>
2021-10-04 11:24:55 -0300

    refactor: Moves OrderAnalyzer logic into TrackOrders

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,16 +1,29 @@
-from src.Order_Analyzer import OrderAnalyzer
+import csv
+from src.track_orders import TrackOrders
+
+
+def get_orders_data(path_to_file):
+    field_names = ['customer', 'dish', 'week_day']
+    with open(path_to_file) as file:
+        reader = csv.DictReader(file, field_names)
+        data = []
+        for row in reader:
+            data.append(row)
+    return data
 
 
 def analyze_log(path_to_file):
-    analyzer = OrderAnalyzer(path_to_file)
+    orders_data = get_orders_data(path_to_file)
+    analyzer = TrackOrders(orders_data)
     with open('data/mkt_campaign.txt', 'w') as file:
-        file.write(analyzer.most_ordered_by_client_name('maria') + '\n')
+        file.write(analyzer.get_most_ordered_dish_per_costumer('maria') + '\n')
         file.write(str(
-            analyzer.quantity_of_dish_by_client_name('arnaldo', 'hamburguer'))
+            analyzer.get_quantity_of_dish_per_customer('arnaldo', 'hamburguer')
+            )
             + '\n')
         file.write(str(
-            analyzer.never_occurred_data_by_client_name('joao', 'dish_name'))
+            analyzer.get_never_ordered_per_costumer('joao'))
             + '\n')
         file.write(str(
-            analyzer.never_occurred_data_by_client_name('joao', 'week_day'))
+            analyzer.get_days_never_visited_per_costumer('joao'))
             + '\n')
