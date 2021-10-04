34892c44143d9b801f8b1e21df48fd0444329115
Lucas Nascimento - MACOS <lucasmvnascimento@gmail.com>
2021-10-04 18:35:38 -0300

    req 02

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,56 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+        self.menu = set()
+        self.daysOfWeek = set()
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
+        self.menu.add(order)
+        self.daysOfWeek.add(day)
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        costumerOrders = {}
+        for order in self.orders:
+            if (order[0] == costumer):
+                if (order[1] not in costumerOrders):
+                    costumerOrders[order[1]] = 1
+                else:
+                    costumerOrders[order[1]] += 1
+        # https://www.kite.com/python/answers/how-to-find-the-max-value-in-a-dictionary-in-python#get
+        return max(costumerOrders, key=costumerOrders.get)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        costumerOrders = set()
+        for order in self.orders:
+            if (order[0] == costumer):
+                costumerOrders.add(order[1])
+        return self.menu.difference(costumerOrders)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        costumerOrderDays = set()
+        for order in self.orders:
+            if (order[0] == costumer):
+                costumerOrderDays.add(order[2])
+        return self.daysOfWeek.difference(costumerOrderDays)
 
     def get_busiest_day(self):
-        pass
+        ordersByDay = {}
+        for order in self.orders:
+            if (order[2] not in ordersByDay):
+                ordersByDay[order[2]] = 1
+            else:
+                ordersByDay[order[2]] += 1
+        return max(ordersByDay, key=ordersByDay.get)
 
     def get_least_busy_day(self):
-        pass
+        ordersByDay = {}
+        for order in self.orders:
+            if (order[2] not in ordersByDay):
+                ordersByDay[order[2]] = 1
+            else:
+                ordersByDay[order[2]] += 1
+        return min(ordersByDay, key=ordersByDay.get)
