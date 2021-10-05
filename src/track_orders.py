1e98bd1ff736c5df1e8b0fa5e0782cbf5675cb24
enicio <jessandro42@gmail.com>
2021-10-05 10:49:55 -0300

    Requisito 2.4

--- src/track_orders.py
+++ src/track_orders.py
@@ -17,13 +17,13 @@ class TrackOrders:
 
     def get_never_ordered_per_costumer(self, costumer):
         rangos = set()
-        orders_costumer = set()
+        order_costumer = set()
         for prato in self.orders:
             rangos.add(prato[1])
         for prato in self.orders:
             if prato[0] == costumer:
-                orders_costumer.add(prato[1])
-        return rangos.difference(orders_costumer)
+                order_costumer.add(prato[1])
+        return rangos.difference(order_costumer)
 
     def get_days_never_visited_per_costumer(self, costumer):
         pass
