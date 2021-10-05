2ece5a3025670e9ec1664fff7814a600aef31376
Silvio Dayube <scarige@gmail.com>
2021-10-05 16:01:59 -0300

    req(03): Implements InventoryControl (partial)

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,25 +1,34 @@
 class InventoryControl:
     INGREDIENTS = {
-        'hamburguer': ['pao', 'carne', 'queijo'],
-        'pizza': ['massa', 'queijo', 'molho'],
-        'misto-quente': ['pao', 'queijo', 'presunto'],
-        'coxinha': ['massa', 'frango'],
+        "hamburguer": ["pao", "carne", "queijo"],
+        "pizza": ["massa", "queijo", "molho"],
+        "misto-quente": ["pao", "queijo", "presunto"],
+        "coxinha": ["massa", "frango"],
     }
     MINIMUM_INVENTORY = {
-        'pao': 50,
-        'carne': 50,
-        'queijo': 100,
-        'molho': 50,
-        'presunto': 50,
-        'massa': 50,
-        'frango': 50,
+        "pao": 50,
+        "carne": 50,
+        "queijo": 100,
+        "molho": 50,
+        "presunto": 50,
+        "massa": 50,
+        "frango": 50,
     }
 
     def __init__(self):
-        pass
+        self.inventory = self.MINIMUM_INVENTORY.copy()
+        self.order_list = []
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.order_list.append(
+            {"costumer": costumer, "order": order, "day": day}
+        )
+
+        for ingredient in self.INGREDIENTS[order]:
+            self.inventory[ingredient] -= 1
 
     def get_quantities_to_buy(self):
-        pass
+        minimum = self.MINIMUM_INVENTORY
+        current = self.inventory
+
+        return {k: minimum[k] - current[k] for k in list(minimum.keys())}
