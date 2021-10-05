bea6116d1c011803b1c34e9580e22e2f2d54378e
Cleberton Francisco <clebertonfgc@gmail.com>
2021-10-05 08:42:26 -0300

    Requisito 2 parcial

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,18 +1,39 @@
+from statistics import mode
+# https://www.geeksforgeeks.org/python-statistics-mode-function/
+
+
 class TrackOrders:
     def __len__(self):
-        ...
+        return len(self.orders)
+
+    def __init__(self):
+        self.orders = []
 
     def add_new_order(self, costumer, order, day):
-        pass
+        return self.orders.append(
+            {"name": costumer, "order": order, "day": day}
+        )
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        meals = []
+        for order in self.orders:
+            if order[0] == costumer:
+                meals.append(order[1])
+
+        return mode(meals)
 
     def get_never_ordered_per_costumer(self, costumer):
         pass
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        days_customer = set()
+        costumer_frequency = set()
+
+        for row in self.orders:
+            if row["name"] == costumer:
+                costumer_frequency.add(row["day"])
+            days_customer.add(row["day"])
+        return days_customer.difference(costumer_frequency)
 
     def get_busiest_day(self):
         pass
