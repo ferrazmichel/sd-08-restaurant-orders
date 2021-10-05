22a503ce174f408465a883541b2d3ba289ed11d4
Silvio Dayube <scarige@gmail.com>
2021-10-05 16:58:58 -0300

    req(04): Implements InventoryControl

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -18,8 +18,13 @@ class InventoryControl:
     def __init__(self):
         self.inventory = self.MINIMUM_INVENTORY.copy()
         self.order_list = []
+        self.available_dishes = set(self.INGREDIENTS.keys())
+        self.available_ingredients = set(self.inventory.keys())
 
     def add_new_order(self, costumer, order, day):
+        if order not in self.get_available_dishes():
+            return False
+
         self.order_list.append(
             {"costumer": costumer, "order": order, "day": day}
         )
@@ -27,8 +32,18 @@ class InventoryControl:
         for ingredient in self.INGREDIENTS[order]:
             self.inventory[ingredient] -= 1
 
+            if self.inventory[ingredient] < 1:
+                self.available_ingredients.remove(ingredient)
+
     def get_quantities_to_buy(self):
         minimum = self.MINIMUM_INVENTORY
         current = self.inventory
 
         return {k: minimum[k] - current[k] for k in list(minimum.keys())}
+
+    def get_available_dishes(self):
+        for dish in self.available_dishes.copy():
+            if not set(self.INGREDIENTS[dish]) <= self.available_ingredients:
+                self.available_dishes.remove(dish)
+
+        return self.available_dishes
