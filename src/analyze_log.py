4100eabfbed67e7793cf8fc2707c40dcca2f6130
Igor <igorln96@gmail.com>
2021-10-04 23:05:58 -0300

    req 1 e 2

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,68 @@
-def analyze_log(path_to_file):
-    raise NotImplementedError
+import csv
 
+def read_csv_file(path):
+    data = []
+    with open(path) as file:
+        read = csv.reader(file, delimiter=',', quotechar='"')
+        for row in read:
+            costumer, order, day = row
+            data.append(
+                {'costumer': costumer, 'order': order, 'day': day}
+            )
+
+    return data
+
+def most_popular(client, orders):
+    food = {}
+
+    for order in orders:
+        if client == order['costumer']:
+            if order['order'] not in food:
+                food[order['order']] = 1
+            else:
+                food[order['order']] += 1
+    return max(food, key=food.get)
+
+def number_of_orders(client, orders):
+    food = {}
+
+    for order in orders:
+        if client == order['costumer']:
+            if order['order'] not in food:
+                food[order['order']] = 1
+            else:
+                food[order['order']] += 1
+    return food['hamburguer']
+
+def never_ordered(client, orders):
+    order_set = set()
+    all_orders = set()
+    for order in orders:
+        all_orders.add(order['order'])
+    for order in orders:
+        if client == order['costumer']:
+            order_set.add(order['order'])
+    return all_orders - order_set
+
+def never_have_been(client, orders):
+    date = set()
+    all_date = set()
+    for order in orders:
+        all_date.add(order['day'])
+    for order in orders:
+        if client == order['costumer']:
+            date.add(order['day'])
+    return all_date - date
+
+def write_file(content):
+    with open('data/mkt_campaign.txt', 'w') as file:
+        file.write(content)
+
+def analyze_log(path_to_file):
+    data = read_csv_file(path_to_file)
+    most_ordered = most_popular('maria', data)
+    number_of_order = number_of_orders('arnaldo', data)
+    never = never_ordered('joao', data)
+    never_went = never_have_been('joao', data)
+    content = f"{most_ordered}\n{number_of_order}\n{never}\n{never_went}\n"
+    write_file(content)
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,28 @@
+from src.analyze_log import (
+    most_popular, never_have_been, never_ordered 
+)
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append({'costumer': costumer, 'order': order, 'day': day})
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return most_popular(costumer, self.orders)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return never_ordered(costumer, self.orders)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return never_have_been(costumer, self.orders)
 
     def get_busiest_day(self):
         pass
-
     def get_least_busy_day(self):
         pass
