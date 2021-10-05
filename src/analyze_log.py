75b90e0c0437ddd9dd1274b67c3b582ab9c9ac38
JeanVictorMachado <jeanvictor.arq@gmail.com>
2021-10-04 21:34:25 -0300

    implementing trackOrders class

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,53 +1,29 @@
 import csv
-from collections import Counter
+from src.track_orders import TrackOrders
 
 
-class Order_Analyzer:
-    def __init__(self, path_to_file):
-        self.field_names = ['client_name', 'dish_name', 'week_day']
-        with open(path_to_file) as file:
-            reader = csv.DictReader(file, self.field_names)
-            data = []
-            for row in reader:
-                data.append(row)
-        self.data = data
-
-    def _orders_by_client_name(self, name):
-        return [
-            {key: order[key] for key in order if key != 'client_name'}
-            for order in self.data
-            if order['client_name'] == name
-        ]
-
-    def _dishes_counter_by_client_name(self, name):
-        client_orders = self._orders_by_client_name(name)
-        return Counter([order['dish_name'] for order in client_orders])
-
-    def most_ordered_by_client_name(self, name):
-        dishes_counter = self._dishes_counter_by_client_name(name)
-        return max(dishes_counter.items(), key=lambda tup: tup[1])[0]
-
-    def quantity_of_dish_by_client_name(self, name, dish):
-        dishes_counter = self._dishes_counter_by_client_name(name)
-        return dishes_counter.get(dish, 0)
-
-    def never_occurred_data_by_client_name(self, name, field_name):
-        all = {order[field_name] for order in self.data}
-        client_orders = self._orders_by_client_name(name)
-        client_occurrences = {order[field_name] for order in client_orders}
-        return all - client_occurrences
+def get_orders_data(path_to_file):
+    field_names = ['customer', 'dish', 'week_day']
+    with open(path_to_file) as file:
+        reader = csv.DictReader(file, field_names)
+        data = []
+        for row in reader:
+            data.append(row)
+    return data
 
 
 def analyze_log(path_to_file):
-    analyzer = Order_Analyzer(path_to_file)
+    orders_data = get_orders_data(path_to_file)
+    analyzer = TrackOrders(orders_data)
     with open('data/mkt_campaign.txt', 'w') as file:
-        file.write(analyzer.most_ordered_by_client_name('maria') + '\n')
+        file.write(analyzer.get_most_ordered_dish_per_costumer('maria') + '\n')
         file.write(str(
-            analyzer.quantity_of_dish_by_client_name('arnaldo', 'hamburguer'))
+            analyzer.get_quantity_of_dish_per_customer('arnaldo', 'hamburguer')
+            )
             + '\n')
         file.write(str(
-            analyzer.never_occurred_data_by_client_name('joao', 'dish_name'))
+            analyzer.get_never_ordered_per_costumer('joao'))
             + '\n')
         file.write(str(
-            analyzer.never_occurred_data_by_client_name('joao', 'week_day'))
+            analyzer.get_days_never_visited_per_costumer('joao'))
             + '\n')
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,65 @@
+from collections import Counter
+
+
+def without_keys(dct, keys):
+    dct = dct.copy()
+    for key in keys:
+        dct.pop(key)
+    return dct
+
+
 class TrackOrders:
+    def __init__(self, data=[]):
+        self.data = list(data)
+
     def __len__(self):
-        pass
+        return len(self.data)
 
-    def add_new_order(self, costumer, order, day):
-        pass
+    def _orders_by_customer(self, customer):
+        return [
+            without_keys(order, ['customer'])
+            for order in self.data
+            if order['customer'] == customer
+        ]
 
-    def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+    def _field_counter(self, field_name):
+        return Counter([order[field_name] for order in self.data])
 
-    def get_never_ordered_per_costumer(self, costumer):
-        pass
+    def _field_counter_by_customer(self, customer, field_name):
+        customer_orders = self._orders_by_customer(customer)
+        return Counter([order[field_name] for order in customer_orders])
 
-    def get_days_never_visited_per_costumer(self, costumer):
-        pass
+    def _never_occurred_data_by_customer(self, customer, field_name):
+        all = {order[field_name] for order in self.data}
+        customer_orders = self._orders_by_customer(customer)
+        customer_occurrences = {order[field_name] for order in customer_orders}
+        return all - customer_occurrences
+
+    def add_new_order(self, customer, dish, week_day):
+        self.data.append({
+            'customer': customer,
+            'dish': dish,
+            'week_day': week_day,
+        })
+
+    def get_most_ordered_dish_per_costumer(self, customer):
+        dishes_counter = self._field_counter_by_customer(customer, 'dish')
+        return max(dishes_counter.items(), key=lambda tup: tup[1])[0]
+
+    def get_quantity_of_dish_per_customer(self, customer, dish):
+        dishes_counter = self._field_counter_by_customer(customer, 'dish')
+        return dishes_counter.get(dish, 0)
+
+    def get_never_ordered_per_costumer(self, customer):
+        return self._never_occurred_data_by_customer(customer, 'dish')
+
+    def get_days_never_visited_per_costumer(self, customer):
+        return self._never_occurred_data_by_customer(customer, 'week_day')
 
     def get_busiest_day(self):
-        pass
+        day_counter = self._field_counter('week_day')
+        return max(day_counter.items(), key=lambda tup: tup[1])[0]
 
     def get_least_busy_day(self):
-        pass
+        day_counter = self._field_counter('week_day')
+        return min(day_counter.items(), key=lambda tup: tup[1])[0]
