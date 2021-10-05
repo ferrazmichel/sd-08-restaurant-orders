e062e5f87472dfa73179f1041388f2f5ff95c44e
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-10-05 11:05:50 -0300

    all reqs until 80 pls
    Signed-off-by: Bruno Pedrosa <bgpedrosas@gmail.com>

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,86 @@
+def person_orders(person_list):
+    food_amount = {}
+    
+    for food in person_list:
+        try:
+            food_amount[food["order"]] += 1
+        except KeyError:
+            food_amount[food["order"]] = 1
+    
+    return food_amount
+
+
+def person_dates(person_list):
+    date_amount = {}
+    
+    for date in person_list:
+        try:
+            date_amount[date["day"]] += 1
+        except KeyError:
+            date_amount[date["day"]] = 1
+    
+    return date_amount
+
+
+def never_ordered_joao(joao, available_order):
+    not_ordered = []
+    joao_foods = person_orders(joao)
+
+    for order in available_order:
+        try:
+            bool(joao_foods[order])
+        except KeyError:
+            not_ordered.append(order)
+    return set(not_ordered)
+
+
+def never_frequented_joao(joao):
+    days = [
+        "segunda-feira",
+        "terça-feira",
+        "quarta-feira",
+        "quinta-feira",
+        "sexta-feira", 
+        "sabado", 
+        "domingo", 
+    ]
+    not_frequented = []
+    joao_dates = person_dates(joao)
+
+    for day in days:
+        try:
+            bool(joao_dates[day])
+        except KeyError:
+            not_frequented.append(day)
+    return set(not_frequented)
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    with open(path_to_file) as file:
+        orders_by_person = {}
+        available_orders = []
+        lines = file.readlines()
+        for line in lines:
+            [name, order, day] = line.rstrip().split(",")
+            available_orders.append(order)
+            try:
+                orders_by_person[name].append({"order": order, "day": day})
+            except KeyError:
+                orders_by_person[name] = [{"order": order, "day": day}]
+        available_orders = set(available_orders)
+        maria_orders = person_orders(orders_by_person["maria"])
+        maria_answer = max(maria_orders, key=maria_orders.get)
+
+        arnaldo_orders = person_orders(orders_by_person["arnaldo"])
+        arnaldo_answer = arnaldo_orders["hamburguer"]
+
+        joao_order_answer = never_ordered_joao(orders_by_person["joao"], available_orders)
+        joao_day_answer = never_frequented_joao(orders_by_person["joao"])
+
+        result = open('data/mkt_campaign.txt', 'w')
+        result.write(
+            f"{maria_answer}\n"
+            f"{arnaldo_answer}\n"
+            f"{joao_order_answer}\n"
+            f"{joao_day_answer}"
+        )
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,44 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        food_amount = {}
+        for order in self.orders:
+            [name, order_prod, _] = order
+            if costumer == name:
+                try:
+                    food_amount[order_prod] += 1
+                except KeyError:
+                    food_amount[order_prod] = 1
+
+        return max(food_amount, key=food_amount.get)
 
     def get_never_ordered_per_costumer(self, costumer):
         pass
 
     def get_days_never_visited_per_costumer(self, costumer):
         pass
+    
+    def orders_count(self):
+        days_orders = {}
+        for [_, _, day] in self.orders:
+            try:
+                days_orders[day] += 1
+            except KeyError:
+                days_orders[day] = 1
+        return days_orders
 
     def get_busiest_day(self):
-        pass
-
+        orders_amount = self.orders_count()
+        return max(orders_amount, key=orders_amount.get)
+    
     def get_least_busy_day(self):
-        pass
+        orders_amount = self.orders_count()
+        return min(orders_amount, key=orders_amount.get)
