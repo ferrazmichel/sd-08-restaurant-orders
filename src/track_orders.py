69795f7ec8bc7ae42191b06164f5be5232df2038
Cleberton Francisco <clebertonfgc@gmail.com>
2021-10-05 09:08:34 -0300

    Outras funções

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,7 +1,11 @@
 from statistics import mode
+import collections
+
 # https://www.geeksforgeeks.org/python-statistics-mode-function/
 # https://www.geeksforgeeks.org/python-set-difference/#:~:text=The%20difference%20between%20the%20two,the%20difference%20between%20two%20sets.&text=We%20can%20also%20use%20%E2%80%93%20operator,the%20difference%20between%20two%20sets.
 # https://www.w3schools.com/python/ref_set_difference.asp
+# https://stackoverflow.com/questions/3594514/how-to-find-most-common-elements-of-a-list/44481414
+
 
 class TrackOrders:
     def __len__(self):
@@ -44,7 +48,17 @@ class TrackOrders:
         return days_customer.difference(costumer_frequency)
 
     def get_busiest_day(self):
-        pass
+        days_busiest = []
+
+        for row in self.orders:
+            days_busiest.append(row["day"])
+
+        return collections.Counter(days_busiest).most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        days_least = []
+
+        for row in self.orders:
+            days_least.append(row["day"])
+
+        return collections.Counter(days_least).most_common()[-1][0]
