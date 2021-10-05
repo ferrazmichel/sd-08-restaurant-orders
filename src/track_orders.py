ff7a15e3baa5e16b355d0c43332a542b5796f6a4
enicio <jessandro42@gmail.com>
2021-10-05 10:42:20 -0300

    Requisito 2.3 fix linter

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,18 +1,29 @@
 class TrackOrders:
     def __init__(self):
-        self.orders = {}
+        self.orders = []
 
     def __len__(self):
         return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        orders = []
+        for prato in self.orders:
+            if prato[0] == costumer:
+                orders.append(prato[1])
+        return max(set(orders), key=orders.count)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        rangos = set()
+        orders_costumer = set()
+        for prato in self.orders:
+            rangos.add(prato[1])
+        for prato in self.orders:
+            if prato[0] == costumer:
+                orders_costumer.add(prato[1])
+        return rangos.difference(orders_costumer)
 
     def get_days_never_visited_per_costumer(self, costumer):
         pass
